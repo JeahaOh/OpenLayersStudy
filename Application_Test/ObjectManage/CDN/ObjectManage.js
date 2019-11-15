07dc2ac9d6b96bd4f1ce2ddd65acfaac5cba69eb
JeahaOh <jeaha1217@gmail.com>
2019-11-15 16:04:23 +0900

    Ojbect Manage : BETA VERSION

--- Application_Test/ObjectManage/CDN/ObjectManage.js
+++ Application_Test/ObjectManage/CDN/ObjectManage.js
@@ -52,7 +52,9 @@ let map = new ol.Map({
 //  <-- 기본 맵 설정.
 
 //  Global
-let objDraw, circleRular, objSnap, objModify, measureTooltipElement, coordTooltipElement;
+let testGlobal;
+let objDraw, objSnap, objModify, measureTooltipElement, pointTooltip, pointTooltipElement, pointCoord, measureCircle, status, pointCNT;
+let lineDistance = '';
 
 //  Line을 그리면 사각형으로 변환한 geometry를 반환한다.
 let squareFunction = function( coordinates, geometry ) {
@@ -70,14 +72,14 @@ let squareFunction = function( coordinates, geometry ) {
 
 /**
  * @param {*} flag : init 함수를 어디서 호출하는지 확인하기 위한 구분자.
- * true 라면 거리 재기가 끝났을 때 호출,
- * false 라면 거리 재기를 하지 않았을 때 호출.
+ * true 라면 측정이 끝났을 때 호출,
+ * false 라면 측정을 하지 않았을 때 호출.
  * 
  * true일 때 호출 한 경우
  * 리스트에 선택 된 항목을 선택 해제한다.
  * 
  * false일 때 호출 한 경우
- * 거리 재기에 사용된 자원들을 초기화 함.
+ * 측정에 사용된 자원들을 초기화 함.
  * 
  * 공통적으로
  * map에 있는 Draw, Snap, Modify Interaction을 지운다.
@@ -95,8 +97,11 @@ const objMngInit = function( flag ){
     map.removeLayer( rularVector );
     rularSource.clear();
     $('div').remove('.rular-point');
+    map.removeOverlay( pointTooltip );
   }
-  
+  //  측정을 하던 안하던 여기서 init 해야 함.
+  map.un('click', rularClick );
+  lineDistance = '';
   map.removeInteraction( objDraw );
   map.removeInteraction( objSnap );
   map.removeInteraction( objModify );
@@ -121,6 +126,109 @@ const createMeasureTooltip = function() {
 };  //  createMeasureTooltip
 
 /**
+ * 인자로 받은 좌표 지점에 측정 Overlay와 Point Feature를 찍어주는 함수.
+ * @param {*} coord [14365024.794099694, 4219904.888355112] ('EPSG:4326')형식의 좌표
+ */
+const pointFunc = function(coord, feature) {
+  console.group('Draw Point Func');
+  
+  let sectionDistance, degree;
+
+  //  구간 거리를 측정하기
+  if( pointCoord ) {
+    sectionDistance = new ol.geom.LineString([ pointCoord, coord ]);
+    sectionDistance = formatLength(sectionDistance)
+  }
+  pointCoord = coord;
+  // console.log( sectionDistance );
+
+  //  구간 측정 Overlay 준비
+  pointTooltipElement = document.createElement('div');
+  className = 'ol-tooltip ol-tooltip-static rular-point';
+  pointTooltipElement.className = status != 'ESL' ? className : className + ' ESL' + pointCNT;
+  if( pointCNT == 0 ) pointTooltipElement.id = 'target';
+  pointTooltip = new ol.Overlay({
+    element: pointTooltipElement,
+    offset: [0, -15],
+    positioning: 'bottom-center'
+  });
+
+  //  죄표를 3957로 변환
+  // console.log( coord )
+  let stringCoord = ol.proj.toLonLat(coord);
+  // console.log( stringCoord );
+  
+  //  좌표를 DMS 로 변환
+  let _lat = toDegreesMinutesAndSeconds( stringCoord[0] ) + 'N';
+  // console.log( _lat );
+  let _lon = toDegreesMinutesAndSeconds( stringCoord[1] ) + 'E';
+  // console.log( _lon );
+  stringCoord = _lat + '&ensp; &ensp;' + _lon;
+
+  
+  if( feature ) {
+    // console.log( feature )
+    feature.getGeometry().transform('EPSG:3857', 'EPSG:4326');
+    coordinates = feature.getGeometry().getCoordinates();
+    point0 = turf.point( coordinates[0] );
+    point1 = turf.point( coordinates[1] );
+    degree = turf.bearing( point0, point1 );
+    degree = Math.round( degree * 100 ) / 100;
+    degree += '°';
+    feature.getGeometry().transform('EPSG:4326', 'EPSG:3857');
+  }
+
+
+
+  //  Overlay에 보여줄 측정 데이터 문자열 정리
+  let display;
+  switch( status ) {
+    case 'Rular' :
+      display =
+        lineDistance == '' ? stringCoord : stringCoord + '<br>'
+        + (sectionDistance === lineDistance 
+          ? '총 거리 : ' + sectionDistance
+          : '총 거리 : ' + lineDistance + '<br>' + '구간 거리 : ' + sectionDistance);
+      break;
+    case 'ESL' :
+      display =  pointCNT == 0 ? stringCoord : stringCoord + '<br>' + sectionDistance;
+      if( document.getElementById('target') ) {
+        target = document.getElementById('target');
+        origin = target.innerText;
+        target.innerHTML = origin + '<br>' + degree ;
+      
+      }
+  }
+
+  // console.log( pointCNT );
+
+  //  Overlay 띄우기
+  pointTooltipElement.innerHTML = display;
+  pointTooltip.setPosition(coord);
+  map.addOverlay(pointTooltip);
+
+  //  클릭 포인트를 잘보이게 하기 위해 Point Feature를 띄움.
+  // console.log(coord);
+  let point = new ol.Feature({
+    geometry: new ol.geom.Point(
+      ol.proj.fromLonLat(coord, 'EPSG:4326', 'EPSG:3857')
+    )
+  });
+  point.setStyle( new ol.style.Style({
+    image: new ol.style.Circle({
+      radius: 10,
+      fill: new ol.style.Fill({
+        color: 'rgba(0, 0, 0, 0.2)'
+      })
+    })
+  }) );
+  rularSource.addFeature( point );
+  pointCNT++;
+  console.groupEnd('Draw Point Func');
+};  //  pointFunc
+
+
+/**
  * Format length output.
  * @param {LineString} line The line.
  * @return {string} The formatted length.
@@ -139,38 +247,19 @@ let formatLength = function(line) {
   return output;
 };  //  formatLength
 
-/**
- * 인자로 받은 좌표 지점에 Point Feature를 찍어주는 함수.
- * @param {*} coord [14365024.794099694, 4219904.888355112] ('EPSG:4326')형식의 좌표
- */
-const drawPoint = function(coord) {
-  console.group('Draw Point Func');
-  // console.log(coord);
-  let point = new ol.Feature({
-    geometry: new ol.geom.Point(
-      ol.proj.fromLonLat(coord, 'EPSG:4326', 'EPSG:3857')
-    )
-  });
-  point.setStyle( new ol.style.Style({
-    image: new ol.style.Circle({
-      radius: 10,
-      fill: new ol.style.Fill({
-        color: 'rgba(0, 0, 0, 0.2)'
-      })
-    })
-  }) );
-  rularSource.addFeature( point );
-  console.groupEnd('Draw Point Func');
-};  //  drawPoint
 
-const rularClick = function( evt ) {  }
+const rularClick = function( evt ) { 
+  // console.log( evt );
+  // console.log( evt.coordinate );
+  pointFunc(evt.coordinate);
+}
 
 const objMng = function( evt ) {
   //  map의 interaction들을 초기화.
   objMngInit();
 
   //  변수 선언
-  let sketch, status, listener;
+  let sketch, listener, maxPoints;
   let className = 'selectedType';
   let menuList = $('#obj_mng_li li');
 
@@ -193,8 +282,8 @@ const objMng = function( evt ) {
   //  objMng 작동할 type
   let type = evt.dataset.val;
   
-  let geometryFunction, maxPoints;
   //  type에 따라 switch
+  let geometryFunction;
   switch( type ) {
     case 'None':
       return;
@@ -206,15 +295,17 @@ const objMng = function( evt ) {
       type = 'Circle';
       geometryFunction = squareFunction;
       break;
+    case 'ESL':
+      status = type;
+      type = 'Circle';
+      maxPoints = 3;
+      pointCNT = 0;
+      break;
     case 'Rular':
       //  거리를 잴 경우 LineString으로 Feature를 그리고, 분기처리를 위해 변수 선언.
+      status = type;
       type = 'LineString';
-      maxPoints = 2;
-      status = true;
-      break;
-    case 'ESL':
-      type = 'Circle';
-      status = true;
+      pointCNT = 0;
       break;
   }
 
@@ -234,7 +325,7 @@ const objMng = function( evt ) {
     map.addLayer(rularVector);
   }
   //  snap 넣어줌.
-  objSnap = new ol.interaction.Snap({ source: objSource});
+  objSnap = new ol.interaction.Snap({ source: objSource });
   map.addInteraction( objSnap );
 
   objDraw.on('drawstart', function( evt ) {
@@ -242,16 +333,9 @@ const objMng = function( evt ) {
 
     //  type이 rular일 경우.
     if( status ) {
-      //  거리재기 시작점에 점 찍어줌.
-      // console.log( evt.feature.getGeometry().getCoordinates()[0] );
-      if( type == 'LineString') drawPoint(evt.feature.getGeometry().getCoordinates()[0]);
-      if( type == 'Circle') {
-        // map.on('click', eslClick);
-        drawPoint( evt.feature.getGeometry().getCenter() );
-        // drawPoint( evt.feature.getFirstCoordinate() );
-        console.log( evt.feature.getGeometry().getCenter() );
-      }
-      //  거리재기 시 클릭마다 rularFunction을 사용함.
+      //  측정 시 클릭마다 측정하기 위한 함수.
+      if( type == 'LineString') map.on('click', rularClick );
+      if( type == 'Circle') pointFunc( evt.feature.getGeometry().getCenter() );
       
       // set sketch
       sketch = evt.feature;
@@ -259,18 +343,21 @@ const objMng = function( evt ) {
       let tooltipCoord = evt.coordinate;
       listener = sketch.getGeometry().on('change', function (evt) {
         let geom = evt.target;
-        console.log('geom');
-        console.log( geom );
-        console.log( geom.getLastCoordinate() );
         let output;
         if (geom instanceof ol.geom.LineString) {
           output = formatLength(geom);
+          lineDistance = output;
           tooltipCoord = geom.getLastCoordinate();
         }
         measureTooltipElement.innerHTML = output;
         measureTooltip.setPosition(tooltipCoord);
+        // console.log( geom.getCoordinates() );
+
+        //  원 측정시 maxPoints로 제한하면 마지막 점에 오버레이를 추가해 주지 않으므로 계산을 따로 함.
+        // if( maxPoints && pointCNT >= 2 ) {
+        //   objDraw.finishDrawing();
+        // }
       });
-      map.on('click', rularClick );
       
     }
     console.groupEnd( 'draw start' );
@@ -281,17 +368,11 @@ const objMng = function( evt ) {
 
     //  type이 rular일 경우
     if( status ) {
-      //  거리재기 끝점에 점 찍어줌.
-      if( type == 'LineString' ) drawPoint( evt.feature.getGeometry().getLastCoordinate() );
       if( type == 'Circle' ) {
-        // map.un('click', eslClick);
-        // console.log( evt.type );
-        // console.log( evt.feature );
-        // console.log( evt.target );
-        // console.log( evt.target.sketchCoords_ );
         let radiusLine = new ol.Feature({
           geometry: new ol.geom.LineString(evt.target.sketchCoords_)
         })
+        // radi
         rularSource.addFeature(radiusLine);
         // console.log( radiusLine )
         // console.log( radiusLine.getGeometry() )
@@ -299,15 +380,9 @@ const objMng = function( evt ) {
         try {
         console.log( formatLength(radiusLine.getGeometry()) );
         } catch ( err ) { console.log(err); }
-        drawPoint(radiusLine.getGeometry().getLastCoordinate());
+        pointFunc(radiusLine.getGeometry().getLastCoordinate() , radiusLine);
       }
-      //  거리재기가 끝나면 rularClick 없애줌.
-      map.un('click', rularClick );
 
-      // console.log( evt.feature.getCenter() )
-
-      measureTooltipElement.className = 'ol-tooltip ol-tooltip-static rular-point';
-      measureTooltip.setOffset([0, -7]);
       sketch.setStyle(new ol.style.Style({
         stroke: new ol.style.Stroke({
           color: 'rgba(0, 0, 0, 0.5)',
@@ -315,28 +390,44 @@ const objMng = function( evt ) {
           lineDash: [3, 7]
         })
       }));
+      map.removeOverlay( measureTooltip );
+      
+      // console.log( sketch );
+      // console.log( sketch.getGeometry() )
+
+        
       // unset sketch
       sketch = null;
       // unset tooltip so that a new one can be created
       measureTooltipElement = null;
       createMeasureTooltip();
       ol.Observable.unByKey(listener);
-
-
+      
+      
       status = undefined;
+      lineDistance = '';
     }
     objMngInit( true );
     console.groupEnd( 'draw end' );
-
-    // objModify = new ol.interaction.Modify({ source: rularSource });
-    // map.addInteraction( objModify );
-
   }); //  drawend
 } //  objMng
 
-const eslClick = function(evt) {
-  console.group( 'ESL Click');
-  console.log( evt );
-
-  console.groupEnd( 'ESL Click');
+/**
+ * 3857 좌표를 DMS 좌표로 변환하기 위해서 사용.
+ * lat, lon 따로 계산 해야 함.
+ * @param {*} coordinate lat이나 lon
+ */
+function toDegreesMinutesAndSeconds(coordinate) {
+  var absolute = Math.abs(coordinate);
+  var degrees = Math.floor(absolute);
+  var minutesNotTruncated = (absolute - degrees) * 60;
+  var minutes = Math.floor(minutesNotTruncated);
+  var seconds = Math.floor((minutesNotTruncated - minutes) * 60);
+  
+  return degrees + "° " + minutes + "." + seconds + "'";
 }
+/**
+ * Issue
+ * Snap을 사용하기 때문에, 측정을 할 때 snap이 작동하면, 
+ * 클릭 좌표가 다를 수 있음...
+ */
